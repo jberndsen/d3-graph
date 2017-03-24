d211e2b1788ad497fe1020fe53c6483dd7b91e88
jberndsen <berndsen.jeroen@gmail.com>
2017-03-24 10:28:19 +0100

    src cleanup

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 // NODES
 d3.json('data-nodes.json', data => {
-    var node = d3.select('graph')
-        .append('svg')
-            .attr('width', 800)
-            .attr('height', 600)
-        .selectAll('g')
-        .data()
-        .enter()
-            .append('g')
-            .attr('transform', (d, i) => `translate(0, ${i * 50})`);
+    console.log(data);
+    // var node = d3.select('graph')
+    //     .append('svg')
+    //         .attr('width', 800)
+    //         .attr('height', 600)
+    //     .selectAll('g')
+    //     .data()
+    //     .enter()
+    //         .append('g')
+    //         .attr('transform', (d, i) => `translate(0, ${i * 50})`);
 
-    node.append('rect')
-        .attr('class', 'node');
+    // node.append('rect')
+    //     .attr('class', 'node');
 
-    node.append('text')
-        .attr('y', 20)
-        .text(d => d.name);
+    // node.append('text')
+    //     .attr('y', 20)
+    //     .text(d => d.name);
 });
