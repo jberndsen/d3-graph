577bdcfe6d27cb396ca88aa08834cf9fc15bc9a7
jberndsen <berndsen.jeroen@gmail.com>
2017-03-24 10:50:32 +0100

    adding multiple objects

--- src/app.js
+++ src/app.js
@@ -1,20 +1,39 @@
-// NODES
-d3.json('data-nodes.json', data => {
-    console.log(data);
-    var node = d3.select('.graph')
-        .append('svg')
-            .attr('width', 800)
-            .attr('height', 600)
-        .selectAll('g')
-        .data(data)
-        .enter()
-            .append('g')
-            .attr('transform', (d, i) => `translate(0, ${i * 50})`);
+// read data
+d3.json('data-nodes.json', nodes => {
+    d3.json('data-links.json', links => {
+        // create the SVG
+        const svg = d3
+            .select('.graph')
+            .append('svg')
+                .attr('width', 800)
+                .attr('height', 600);
 
-    node.append('rect')
-        .attr('class', 'node');
+        // create the nodes
+        const node = svg
+            .selectAll('g.nodes')
+            .data(nodes)
+            .enter()
+                .append('g')
+                .attr('transform', (d, i) => `translate(0, ${i * 50})`);
 
-    node.append('text')
-        .attr('y', 20)
-        .text(d => d.goal);
+        node.append('rect')
+            .attr('class', 'nodes');
+
+        node.append('text')
+            .attr('y', 20)
+            .text(d => d.goal);
+
+        // create the graph links
+        const link = svg
+            .selectAll('g.links')
+            .data(links)
+            .enter()
+                .append('g')
+                .attr('transform', (d, i) => `translate(0, ${i * 60})`);
+
+
+        link.append("circle")
+            .attr("r", 10)
+            .style("fill", "red");
+    })
 });
